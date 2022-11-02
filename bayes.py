f5b9b4e432fb36b55517c02078adf00d1eadecb5
rodrigo <rs9789@gmail.com>
2022-11-02 18:39:18 -0300

    add and corrections of Distribuitions

--- bayes.py
+++ bayes.py
@@ -1,13 +1,82 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from functools import reduce
+import random
+from scipy.stats import gaussian_kde
 
 
-class Distribuiton:
+class Distribuition:
     
-    def beta_mix_sim(self):
-        pass
+    def __init__(self, sample = None, x = None):
+        self.__density = None
+        self.__x = x
+        self.__sample = sample
+        self.__normalized = None
+    
+    def beta_mix_sim(self, params_list:list, size:int):
+        """Generates a random sample of a beta mixture
 
+        Args:
+            params_list (list): list of tuples containing the p and beta params. Ex. [(0.5, 100, 50),(0.5, 400, 500)]
+            size (int): sample size
+
+        Returns:
+            np.ndarray: array with n sample of a beta mixture
+        """
+        
+        n_func = len(params_list)
+        prop_sum = reduce(lambda x, y: x+y, [i[0] for i in params_list])
+        if prop_sum != 1.0:
+            print('Sum of lambdas is not equal 1')
+        
+        uniform_sample = np.random.uniform(0,1,size)
+        
+        if n_func == 2:
+            beta_mixture = []
+            
+            for i in range(size):
+                unif = random.uniform(0,1)
+                if unif <= params_list[0][0]:
+                    beta_mixture.append(random.betavariate(params_list[0][1], params_list[0][2]))
+                elif unif > params_list[0][0]:
+                    beta_mixture.append(random.betavariate(params_list[1][1], params_list[1][2]))
+
+            self.__sample = np.array(beta_mixture)
+            self.__x = np.linspace(0,1,size)
+
+    
+    def dist_freq(self):
+        try:
+            dens = gaussian_kde(self.__sample)
+            y = dens(self.__x)
+            self.__density = y
+        except:
+            print('Run beta_mix_sim() first')
+    
+    def normalize(self):
+        total = self.__density.sum()
+        norm = self.__density/total
+        self.__normalized = norm
+          
+    @property
+    def beta_mix(self):
+        return self.__sample
+            
+    @property
+    def density(self):
+        return self.__density
+            
+    @property
+    def x(self):
+        return self.__x
+    
+    @property
+    def normalized(self):
+        return self.__normalized
+
+          
+        
 class Interval:
     
     def __init__(self):
@@ -113,8 +182,3 @@ class Interval:
     @property
     def sup_index(self):
         return self.__sup_index
-    
-    
-
-
- 
